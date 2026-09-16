c098b93601040ec9f77e57e085e1eaec42b5570b
brookiecookie-lab <taralamfred@gmail.com>
2026-09-15 19:05:28 -0500

    Add vNext site configuration token

---
+++ public-vnext/source/assets/site-config.js
@@ -0,0 +1,12 @@
+window.SITE_CONFIG = Object.freeze({
+  brandName: 'A Certain Kind of Everything',
+  brandStatus: 'WORKING_TOKEN',
+  pagesWeKeepUrl: '',
+  palette: {
+    poseidon: '#0B1F33',
+    ivory: '#F6F0E7',
+    ballerinaPink: '#E7BEC8',
+    transformativeTeal: '#1E6A68',
+    amaranthPurple: '#7A2C63'
+  }
+});
